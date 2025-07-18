5400c6f2d0d9c8d7469b11d8817969c1fc799889
Lucas Chaves <67830005+lucasmar236@users.noreply.github.com>
2025-07-18 09:47:00 -0300

    add notification ao produt

--- desafios/desafio-NotificationProduct/src/domain/product/entity/product.spec.ts
+++ desafios/desafio-NotificationProduct/src/domain/product/entity/product.spec.ts
@@ -22,7 +22,7 @@ describe("Product unit tests", () => {
   it("should throw error when price is less than zero and name is empty", () => {
       expect(() => {
           const product = new Product("123", "", -1);
-      }).toThrowError("product: Price must be greater than zero");
+      }).toThrowError("product: Name is required,product: Price must be greater than zero");
   })
 
   it("should change name", () => {
